b1c6ff912f7d6d7766837b63c6489be2f9706f8c
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-04 21:21:40 +1100

    Create routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Create routes for users
+  resources :users
 end
