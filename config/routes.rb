43a8ec9f9fe277342b3e6f09f460f1869911dac1
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-04 21:27:47 +1100

    Remove superfluous files added by generate controller command

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
   # Create routes for users
     # users     GET    /users(.:format)          users#index
     #           POST   /users(.:format)          users#create
