4c632a93fcfaabd2623a8656f7be835b02277996
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-04 23:45:59 +1100

    Add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
     #           PUT    /users/:id(.:format)      users#update
     #           DELETE /users/:id(.:format)      users#destroy
   resources :users
+
+  # Login/logout routes
+  get '/login'  => 'session#new'        # showing the login form
+  post '/login' => 'session#create'     # form submits to here, performs login, redirects
+  delete '/login' => 'session#destroy'  # logout link goes here, perform logout, redirect
 end
