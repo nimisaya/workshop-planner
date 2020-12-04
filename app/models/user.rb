4b0b8911f2b0c6eb44598abea529fd0673cb5684
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-04 22:24:16 +1100

    Encrypt passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+     # Use the bcrypt gem to encrypt passwords when user creates account, and store only the encrypted version in the 'users' table
+    has_secure_password
 end
