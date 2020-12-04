4e734983207bb82445846278716b45febe771bb1
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-04 22:49:44 +1100

    Add logic to ensure email address is valid in create form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
      # Use the bcrypt gem to encrypt passwords when user creates account, and store only the encrypted version in the 'users' table
     has_secure_password
+
+    # Ensure user enters valid & unique email address
+    validates :email, 
+              presence: true, 
+              uniqueness: {case_sensitive: false},
+              length: {minimum: 4, maximum: 254},
+              format: {with: /^(.+)@(.+)$/, message: "Please check the email address. It's invalid."}
 end
