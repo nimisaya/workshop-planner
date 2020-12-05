c9f1dbe549a335e52063ab1beb0180f5ebf3b06d
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-05 23:25:47 +1100

    Add ability to create a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
   end
 
   def create
+    task = Task.new task_params
+    task.save
+    redirect_to task_path(task.id)
   end
 
   def index
@@ -22,4 +26,8 @@ class TasksController < ApplicationController
 
   def destroy
   end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :tools, :time, :num_participants, :private)
+  end # task_params
 end
