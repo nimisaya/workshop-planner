4db11dca7e69562e159229e63ba99eddef8ec35e
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-05 22:18:03 +1100

    Submit create workshop form

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,26 +1,33 @@
 class WorkshopsController < ApplicationController
   def new
     @workshop = Workshop.new
-  end
+  end # new
 
   def create
-  end
+    workshop = Workshop.new workshop_params
+    workshop.save
+    redirect_to workshop_path(workshop.id)
+  end # create
 
   def index
     @workshops = Workshop.all
-  end
+  end # index
 
   def show
     @workshop = Workshop.find params[:id]
-    @workshop_time = seconds_to_string(@workshop.total_time)
-  end
+    @workshop_time = seconds_to_string(@workshop.total_time) unless @workshop.total_time == nil
+  end # show
 
   def edit
-  end
+  end # edit
 
   def update
-  end
+  end # update
 
   def destroy
-  end
-end
+  end # destroy
+
+  def workshop_params
+    params.require(:workshop).permit(:title, :description, :private)
+  end # workshop_params
+end # WorkshopsController
