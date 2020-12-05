7c65448e6ed96fe4054bcea1649ceaf06100a0a5
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-05 23:06:04 +1100

    Add routes for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,15 @@ Rails.application.routes.draw do
   #               PUT    /workshops/:id(.:format)      workshops#update
   #               DELETE /workshops/:id(.:format)      workshops#destroy
   resources :workshops
+
+  # Create routes for tasks
+  #     tasks GET    /tasks(.:format)              tasks#index
+  #           POST   /tasks(.:format)              tasks#create
+  #  new_task GET    /tasks/new(.:format)          tasks#new
+  # edit_task GET    /tasks/:id/edit(.:format)     tasks#edit
+  #      task GET    /tasks/:id(.:format)          tasks#show
+  #           PATCH  /tasks/:id(.:format)          tasks#update
+  #           PUT    /tasks/:id(.:format)          tasks#update
+  #           DELETE /tasks/:id(.:format)          tasks#destroy
+  resources :tasks
 end
