4f8932325c8b2464c4909e63be7058dc1f49300f
Amanda Jarvinen <nimisaya@outlook.com>
2020-12-05 21:26:50 +1100

    Display a workshops information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,23 @@ class ApplicationController < ActionController::Base
           redirect_to login_path
         end # unless
     end # check_if_logged_in
+
+    def seconds_to_string seconds
+        minute = 60
+        hour = minute * 60
+        day = hour * 24
+    
+        # Convert time from seconds to day, hour, minutes, seconds
+        days, remainder = seconds.divmod(day)
+        hours, remainder = remainder.divmod(hour)
+        minutes, remainder = remainder.divmod(minute)
+        seconds = remainder
+    
+        # Add units
+        time = ["#{days} days", "#{hours} hours", "#{minutes} minutes", "#{seconds} seconds"]
+        # TODO: Handle when singular values i.e. 1 day instead of 1 days
+        
+        # Remove empty units and construct string
+        time.reject{|str| str.start_with?("0")}.join(", ")
+    end # seconds_to_string
 end # ApplicationController
--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -10,6 +10,8 @@ class WorkshopsController < ApplicationController
   end
 
   def show
+    @workshop = Workshop.find params[:id]
+    @workshop_time = seconds_to_string(@workshop.total_time)
   end
 
   def edit
